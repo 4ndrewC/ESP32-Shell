939efd4035fb635823a890142f013a8bfcc276e8
4ndrewC <84311342+4ndrewC@users.noreply.github.com>
2024-11-13 02:25:47 -0600

    Update shell.c

--- shell.c
+++ shell.c
@@ -7,7 +7,7 @@ action_t port_actions[LOG_SIZE];
 
 SemaphoreHandle_t write_logs_semaphore;
 
-/* storing pin data */
+/* storing serial data */
 void store(uint32_t compressed[], void *input, int length){
     uint32_t *data = (uint32_t *)input;
     int div = 4/sizeof(input[0]);
@@ -16,15 +16,6 @@ void store(uint32_t compressed[], void *input, int length){
     }
 }
 
-/* unpacking pin data */
-void load(uint32_t compressed[], uint32_t data[], int length, io_type type){
-    int div = 1;
-    if(type==IO_CHAR) div = 4;
-    else if(type==IO_SHORT) div = 2;
-    for(int i = 0; i<length/div; i++){
-        data[i] = compressed[i];
-    } 
-}
 
 /* helper for stripping left and right spaces from command reads */
 void strip(char *str) {
@@ -46,109 +37,57 @@ int get_number(char *command){
 }
 
 // ------------------ FIX --------------------
-// before calling this function, convert the buffer to a union type array
-typedef union {
-    int i;
-    float f;
-    short s;
-    char c;
-    uint32_t u32;
-    uint16_t u16;
-    uint8_t u8;
-} u_type;
-
-void convert_to_string(void* data, char *output, int length, io_type type){
-    
-    for(int i = 0; i<length; i++){
-        char cur[33];
-        if(type==IO_INT){
-            int *temp = data;
-            snprintf(cur, sizeof(cur), "%d", temp[i]);
-        }
-        else if(type==IO_CHAR){
-            char *temp = data;
-            snprintf(cur, sizeof(cur), "%c", temp[i]);
-        }
-        else if(type==IO_SHORT){
-            short *temp = data;
-            snprintf(cur, sizeof(cur), "%d", temp[i]);
-        }
-        else if(type==IO_FLOAT){
-            float *temp = data;
-            snprintf(cur, sizeof(cur), "%f", temp[i]);
-        }
-        else if(type==IO_U32){
-            uint32_t *temp = data;
-            snprintf(cur, sizeof(cur), "%d" PRIu32, temp[i]);
-        }
-        else if(type==IO_U16){
-            uint16_t *temp = data;
-            snprintf(cur, sizeof(cur), "%u" PRIu16, temp[i]);
-        }
-        else if(type==IO_U8){
-            uint8_t *temp = data;
-            snprintf(cur, sizeof(cur), "%u" PRIu8, temp[i]);
-        }
-        strcat(output, cur);
-    }
-} 
 
 /* update logs on computer */
-// FIX, NEEDS TO SUPPORT ALL DATA TYPES
+
+/* 2 bytes for length, 1 byte for port, 1 byte for io,
+   4 bytes for comm, 256 bytes for data*/
 void write_serial_logs(void *pvParameter){
 
     for(int i = 0; i<log_index; i++){
-        char *port = (char *)malloc(sizeof(char));
+        uint8_t uart_buff[BUF_SIZE + 15];
+
+        uint8_t port;
         switch(port_actions[i].port){
             case UART_NUM_0: 
-                port = "0"; break;
+                port = 0; break;
             case UART_NUM_1: 
-                port = "1"; break;
+                port = 1; break;
             case UART_NUM_2: 
-                port = "2"; break;
+                port = 2; break;
             case UART_NUM_MAX: 
-                port = "3"; break;
+                port = 3; break;
+            default:
+                port = 5; break;
         }
 
-        char *io = (char *)malloc(sizeof(char));
-        io = port_actions[i].io == 1 ? "1" : "0";
-
-        char *dtype = (char *)malloc(5*sizeof(char));
-        // char data[32];
-        switch(port_actions[i].dtype){
-            case IO_INT: 
-                dtype = "int"; break;
-            case IO_SHORT: 
-                dtype = "short"; break;
-            case IO_CHAR: 
-                dtype = "char"; break;
-            case IO_FLOAT: 
-                dtype = "float"; break;
-            case IO_U32: 
-                dtype = "u32"; break;
-            case IO_U16: 
-                dtype = "u16"; break;
-            case IO_U8: 
-                dtype = "u8"; break;
-        }
+        uint8_t io = port_actions[i].io;
 
         char *comm = (char *)malloc(4*sizeof(char));
         comm = port_actions[i].comm == UART ? "uart" : (port_actions[i].comm == I2C ? "i2c" : "spi");
         
-        char *length = (char *)malloc(BUF_SIZE*sizeof(char));
-        snprintf(length, BUF_SIZE, "%d", port_actions[i].length);
+        uint8_t length = port_actions[i].length;
         
-        char *data = (char *)malloc(port_actions[i].length*sizeof(char));
-        data = (char *)port_actions[i].buff;
+        uint8_t *data = (uint8_t *)malloc(port_actions[i].length*sizeof(uint8_t));
+        data = (uint8_t *)port_actions[i].buff;
         
-        strcat(length, port);
-        strcat(length, io);
-        strcat(length, dtype);
-        strcat(length, comm);
-        strcat(length, data);
-        char header[BUF_SIZE+15] = "!serial ";
-        strcat(header, length);
-        uart_write_bytes(UART_NUM_0, (const uint8_t *)header, BUF_SIZE+15);
+        uart_buff[0] = length;
+        uart_buff[1] = port;
+        uart_buff[2] = io;
+        int index = 3;
+        while(*comm!='\0'){
+            uart_buff[index] = *comm;
+            index++;
+            comm++;
+        } 
+        if(index==6) uart_buff[index] = '0';
+        index = 7;
+        for(int i = 0; i<BUF_SIZE; i++){
+            uart_buff[index] = data[i];
+            index++; 
+        }
+
+        uart_write_bytes(UART_NUM_0, uart_buff, sizeof(uart_buff));
     }
     xSemaphoreGive(write_logs_semaphore);
     vTaskDelete(NULL);
@@ -187,9 +126,13 @@ void port_logs(){
     }
 #endif
 
-/* wrapper for uart write */
-int uart_write(uart_port_t s_port, void *buff, int length, io_type iotype){
+#if (log_serial == 1)
+#define uart_write_bytes(s_port, buff, length) uart_write(s_port, buff, length)
+#define i2c_master_write_to_device(s_port, slave_addr, data, length, ticks_to_wait) i2c_write(s_port, slave_addr, data, length, ticks_to_wait);
+#endif
 
+int uart_write(uart_port_t s_port, void *buff, int length){
+    
     uint32_t *data = buff;
 
     /* write to computer logs if temp log is filled up */
@@ -198,7 +141,6 @@ int uart_write(uart_port_t s_port, void *buff, int length, io_type iotype){
 
     port_actions[log_index].port = s_port;
     port_actions[log_index].io = 0;
-    port_actions[log_index].dtype = iotype;
     port_actions[log_index].comm = UART;
     
     // this is to make sure the data sent fits perfectly into multiples of 32-bits
@@ -209,11 +151,15 @@ int uart_write(uart_port_t s_port, void *buff, int length, io_type iotype){
 
     log_index++;
 
-    return uart_write_bytes(s_port, (const uint8_t *)buff, length);
+    #undef uart_write_bytes
+    int ret = uart_write_bytes(s_port, (const uint8_t *)buff, length);
+    #define uart_write_bytes(s_port, buff, length) uart_write(s_port, buff, length)
+    
+    return ret;
 }
 
 /* wrapper for i2c write */
-esp_err_t i2c_write(i2c_port_t s_port, uint8_t slave_addr, void *buff, int length, TickType_t ticks_to_wait, io_type iotype){
+esp_err_t i2c_write(i2c_port_t s_port, uint8_t slave_addr, void *buff, int length, TickType_t ticks_to_wait){
 
     uint32_t *data = buff;
 
@@ -223,24 +169,26 @@ esp_err_t i2c_write(i2c_port_t s_port, uint8_t slave_addr, void *buff, int lengt
 
     port_actions[log_index].port = s_port;
     port_actions[log_index].io = 0;
-    port_actions[log_index].dtype = iotype;
     port_actions[log_index].comm = I2C;
     
     // this is to make sure the data sent fits perfectly into multiples of 32-bits
     int mod = length%4;
     port_actions[log_index].length = length+(4-mod);
-    // port_actions[log_index].length = length;
     
 
     store(port_actions[log_index].buff, data, port_actions[log_index].length);
 
     log_index++;
 
-    return i2c_master_write_to_device(s_port, slave_addr, data, sizeof(data), ticks_to_wait);
+    #undef i2c_master_write_to_device
+    esp_err_t ret = i2c_master_write_to_device(s_port, slave_addr, data, sizeof(data), ticks_to_wait);
+    #define i2c_master_write_to_device(s_port, slave_addr, data, length, ticks_to_wait) i2c_write(s_port, slave_addr, data, length, ticks_to_wait);
+
+    return ret;
 }
 
 /* ------------ UNTESTED --------------*/
-void uart_read(uart_port_t s_port, void *buff, int length, io_type iotype, TickType_t ticks_to_wait){
+int uart_read(uart_port_t s_port, void *buff, int length, TickType_t ticks_to_wait){
 
     int ret = uart_read_bytes(s_port, buff, length, ticks_to_wait);
     if(ret==ESP_FAIL) printf("uart failed to read\n");
@@ -251,7 +199,6 @@ void uart_read(uart_port_t s_port, void *buff, int length, io_type iotype, TickT
 
     port_actions[log_index].port = s_port;
     port_actions[log_index].io = 1;
-    port_actions[log_index].dtype = iotype;
     port_actions[log_index].comm = UART;
 
     int mod = length%4;
@@ -260,10 +207,11 @@ void uart_read(uart_port_t s_port, void *buff, int length, io_type iotype, TickT
     store(port_actions[log_index].buff, data, port_actions[log_index].length);
 
     log_index++;
+
+    return ret;
 }
 
-// ------------ WORK ON THIS -------------------
-
+// ---------- TASK LOGGING -----------
 /* 
 to do:
 - uart write instead of just esp log info
@@ -293,8 +241,6 @@ void create_task(TaskFunction_t pxTaskCode, const char * const pcName, const con
         ESP_LOGI("TAG", "Max task limit reached");
         return;
     }
-    #undef xTaskCreate
-    xTaskCreate(pxTaskCode, pcName, usStackDepth, pvParameters, uxPriority, pxCreatedTask);
     // log tasks here
     ESP_LOGI("TAG", "Task %s created through pseudo function\n", pcName);
     int next = get_next_task(); // get next available task index
@@ -303,11 +249,14 @@ void create_task(TaskFunction_t pxTaskCode, const char * const pcName, const con
     task_log[next].priority = uxPriority;
     task_active[next] = 1;
     if(next>task_index) task_index = next;
+
+    #undef xTaskCreate
+    xTaskCreate(pxTaskCode, pcName, usStackDepth, pvParameters, uxPriority, pxCreatedTask);
     #define xTaskCreate(task, name, stack_size, parameters, priority, handle) create_task(task, name, stack_size, parameters, priority, handle)
 }
 
 void delete_task(TaskHandle_t task){
-    #undef vTaskDelete
+
     ESP_LOGI("TAG", "Task deleted through pseudo function\n");
     // unlog task
     TaskHandle_t cur_task = task==NULL? xTaskGetCurrentTaskHandle() : task;
@@ -320,6 +269,8 @@ void delete_task(TaskHandle_t task){
             break;
         }
     }
+
+    #undef vTaskDelete
     vTaskDelete(task);
     #define vTaskDelete(task) delete_task(task)
 }
