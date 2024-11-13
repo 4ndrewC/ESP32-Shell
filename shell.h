a3e3c484046395285b603254c55c113343333e41
4ndrewC <84311342+4ndrewC@users.noreply.github.com>
2024-11-12 23:57:33 -0600

    Create shell.h

---
+++ shell.h
@@ -0,0 +1,22 @@
+/*
+ * Developed on top of ESP-IDF-v5.3.1
+ */
+
+#ifndef SHELL_H
+#define SHELL_H
+
+#define debugger 0
+#define log_tasks 1
+
+/* 
+ * Initializes ESP-32 Shell 
+ * 
+ * Initializes UART to USB bridge at UART_NUM_0 
+ * 
+ * Creates a task called command_read -> reads commands 
+ * through ESP terminal
+ * 
+ */
+void shell_init();
+
+#endif
